adfcd16302b1a621cbc9b2c91cef8d2fcc932887
code-in-the-snow <pscott_74@yahoo.com>
2015-07-16 14:02:59 -0400

    Some stylikng.  Rspec broke.

--- trivia_app/spec/features/user_starts_a_quiz_spec.rb
+++ trivia_app/spec/features/user_starts_a_quiz_spec.rb
@@ -2,6 +2,27 @@ require 'rails_helper'
 
 feature 'user starts a quiz', '''
   As a signed in user
+  I want to see my quiz history
+  So that I can see if my scores are improving
+''' do
+
+# Acceptance Criteria
+# [ ] I must be logged in
+# [ ] I can see a a table of past quizzes
+
+  scenario 'signed in user starts quiz' do
+    user = FactoryGirl.create(:user)
+
+    visit user_path
+
+    expect(page).to have_content('Question 1')
+    expect(page).to have_content('Question 1')
+    expect(page).to have_content('Question 1')
+    expect(page).to have_content('Question 1')
+  end
+
+feature 'user starts a quiz', '''
+  As a signed in user
   I want to start a quiz
   So that I can test my trivia knowledge
 ''' do
