bb15ca8410d0cf86abfead4e7553e9cdceb36dd6
code-in-the-snow <pscott_74@yahoo.com>
2015-07-16 14:22:04 -0400

    git wants a commit

--- trivia_app/spec/features/user_starts_a_quiz_spec.rb
+++ trivia_app/spec/features/user_starts_a_quiz_spec.rb
@@ -1,25 +1,25 @@
 require 'rails_helper'
 
-feature 'user starts a quiz', '''
-  As a signed in user
-  I want to see my quiz history
-  So that I can see if my scores are improving
-''' do
-
-# Acceptance Criteria
-# [ ] I must be logged in
-# [ ] I can see a a table of past quizzes
-
-  scenario 'signed in user starts quiz' do
-    user = FactoryGirl.create(:user)
-
-    visit user_path
-
-    expect(page).to have_content('Question 1')
-    expect(page).to have_content('Question 1')
-    expect(page).to have_content('Question 1')
-    expect(page).to have_content('Question 1')
-  end
+# feature 'user starts a quiz', '''
+#   As a signed in user
+#   I want to see my quiz history
+#   So that I can see if my scores are improving
+# ''' do
+#
+# # Acceptance Criteria
+# # [ ] I must be logged in
+# # [ ] I can see a a table of past quizzes
+#
+#   scenario 'signed in user starts quiz' do
+#     user = FactoryGirl.create(:user)
+#
+#     visit user_path
+#
+#     expect(page).to have_content('Question 1')
+#     expect(page).to have_content('Question 1')
+#     expect(page).to have_content('Question 1')
+#     expect(page).to have_content('Question 1')
+#   end
 
 feature 'user starts a quiz', '''
   As a signed in user
